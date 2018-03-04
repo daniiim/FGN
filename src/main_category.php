d88a9788cc1d85ce4993ea449bb1b9766af60d4c
Dennis <dennis.spierenburg@hotmail.com>
2018-03-04 14:13:09 +0100

    discover category page

--- src/main_category.php
+++ src/main_category.php
@@ -1,6 +1,7 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php');
+$ih = Loader::helper('image');
 $title = $c->getCollectionName();
 $path = $c->getCollectionPath();
 if ($c->getCollectionAttributeValue('category_description')) {
@@ -22,74 +23,191 @@ if ($user_can_place_free_add) {
 } else {
 	$total_available = $available_nr_of_adds;
 }
+$img = $c->getAttribute('header_image');
+if($img){
+    $thumb = $ih->getThumbnail($img, 9999, 9999, false);
+  }
 $categories_home_page = page::getByID(997);
 $total_adds 	= $categories_home_page->getAttribute('nr_adds');
 $total_requests = $categories_home_page->getAttribute('nr_requests');
 
 ?>
-
-<section>
-  <div class="container inner-xxs">
-    <div class="row">
-      <div class="col-md-12"><?php  $this->inc('elements/breadcrumbs.php'); ?>
-        <div class="row">
-          <div class="col-md-6 text-center-mobile">
-      			<a class="hidden-sm hidden-xs btn red btn-light-gray-bordered no-outer-bottom" href="<?php echo $this->url('zoek_resultaten');?>"><i class="icon-search"></i> Bekijk advertenties  (<?php echo $total_adds;?>)</a>
-      			<a class="hidden-md hidden-lg red no-wrap-ellipses" href="<?php echo $this->url('/zoek_resultaten');?>"><i class="icon-search"></i> Advertenties (<?php echo $total_adds;?>)</a>
-      			<a class="hidden-sm hidden-xs btn blue btn-light-gray-bordered no-outer-bottom" href="<?php echo $this->url('zoek_resultaten?search_type=request');?>"><i class="icon-search"></i> Bekijk Opdrachten (<?php echo $total_requests;?>)</a>
-      			<a class="hidden-md hidden-lg blue no-wrap-ellipses" href="<?php echo $this->url('/zoek_resultaten?search_type=request');?>"><i class="icon-search"></i> Opdrachten (<?php echo $total_requests;?>)</a>
-		      </div>
-		      <div class="col-md-6 text-right"><a class="btn btn-red btn-block-mobile no-outer-bottom" id="request-button" href="<?php echo $this->url('/mijn_fgn/advertenties/advertentie_categorie');?>"><i class="icon-pencil"></i> Plaats advertentie <?php if ($u->isLoggedIn()): echo '('.$total_available.')'; endif;?></a>
-            <a class="btn no-outer-bottom btn-block-mobile" href="<?php echo $this->url('/mijn_fgn/opdrachten/opdracht_categorie');?>"><i class="icon-pencil"></i> Plaats opdracht</a>
+<section class="discover">
+  <?php  $this->inc('elements/breadcrumbs.php'); ?>
+  <div class="row header no-margin">
+    <a href="<?php echo $this->url('/mijn_fgn/advertenties/advertentie_categorie');?>">Advertentie</a>
+    <h2>Ontdekken in <?php echo $title; ?></h2>
+    <a href="<?php echo $this->url('/mijn_fgn/opdrachten/opdracht_categorie');?>">Opdracht</a>
+  </div>
+  <div class="row category no-margin">
+    <div class="item small-full medium-two-third columns first">
+      <div class="category_single">
+        <a href="<?php echo $url; ?>" class="full_url"></a>
+        <div class="category_single-image"  style="background-image: url('<?php echo $thumb->src; ?>')">
+        </div>
+        <div class="category_single-footer">
+          <div class="category_single-footer_icon <?php echo $pageName; ?>">
+            <span class="icon"></span>
+          </div>
+          <div class="category_single-footer-text">
+            <h3 class="category_single-footer-text-title"><?php echo $title; ?></h3>
+            <a href="<?php echo $url; ?>" class="category_single-footer-text-url">Categorie bekijken</a>
           </div>
         </div>
       </div>
-	    <div class="col-md-12">
-        <hr class="hr-sm"/>
-	      <a class="btn btn-block btn-border btn-collpase-categories-sidebar" data-toggle="collapse" data-target=".collapse-categories-sidebar"><i class="icon-layout"></i> Blader door categorieën</a>
-      </div>
-      <div class="col-md-3 collapse collapse-categories-sidebar">
-        <?php
-  			$a = new GlobalArea('Categorieën Sidebar');
-  			$a->display();
-		    ?>
-      </div>
-      <div class="col-md-9">
-        <div class="row">
-	        <div class="col-md-7">
-            <h1 class="small">Rubrieken binnen de categorie <?php echo $c->getCollectionName();?></h1>
-	        </div>
-	        <div class="col-md-5 text-right small">
-            <a class="red outer-right-xxs" id="add-total-button" href="<?php echo $this->url('/zoek_resultaten?path='.$path);?>"><i class="icon-search"></i> Advertenties </a>
-            <a class="blue" id="request-total-button" href="<?php echo $this->url('/zoek_resultaten?search_type=request&path='.$path);?>"><i class="icon-search"></i> Opdrachten </a>
-          </div>
-        </div>
-        <?php
-          $a = new GlobalArea('Categorieën Hoofd');
-		      $a->display();
-		    ?>
+    </div>
+    <div class="side small-full medium-third columns hide-small">
+      <div class="category-view">
+        <h3>Rubrieken in <?php echo $title; ?></h3>
+        <?php for ($i = 0; $i < 6; $i++){
+          ?>
+          <a href="#" class="category-view_url">Therapie</a>
+          <?php
+        }?>
       </div>
     </div>
   </div>
 </section>
-<!-- <section id="help-form-section" class="darker-bg">
-  <div class="container inner-xs">
-    <div class="row">
-		<div class="col-sm-8">
-			<section class="light-bg inner-xs inner-left-xs inner-right-xs">
-        <?php
-			$a = new GlobalArea('Contact Help Form');
-			$a->display();
-		?>
-			</section>
-		</div>
-      <div class="col-sm-4">
-        <?php
-			$a = new GlobalArea('Contact Help Intro');
-			$a->display();
-		?>
+<section class="featured_profile">
+  <div class="row no-margin">
+    <div class="columns small-full featured_profile-header">
+      <div class="featured_profile-header_container">
+        <p>
+          Uitgelichte profilen in "<?php echo $title; ?>"
+        </p>
+        <a href="<?php echo $url; ?>" class="featured_profile-header-url">Categorie bekijken</a>
       </div>
     </div>
   </div>
-</section> -->
+  <div class="row no-margin">
+    <div class="columns small-full medium-third featured_profile-account">
+      <div class="featured_profile-account-container">
+        <div class="featured_profile-account-image">
+          <span class="featured_profile-account-image-number">123</span>
+        </div>
+        <h3 class="featured_profile-account-name">John Doe</h3>
+        <div class="featured_profile-account-accountInfo">
+          <p>
+            <a href="#" class="featured_profile-account-accountInfo_adds">2 advertenties</a> in <a href="#" class="featured_profile-account-accountInfo_place">plaatsnaam</a>
+          </p>
+        </div>
+        <div class="featured_profile-account-infoText">
+          <?php $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+          ?>
+          <p>
+            <?php if(strlen($text) > 80){
+              $text = substr($text, 0, 80) . '...';
+            }
+            echo $text;
+            ?>
+          </p>
+        </div>
+        <a class="featured_profile-account-url" href="#">Profiel bekijken</a>
+      </div>
+    </div>
+    <div class="columns small-full medium-third featured_profile-account">
+      <div class="featured_profile-account-container">
+        <div class="featured_profile-account-image">
+          <span class="featured_profile-account-image-number">123</span>
+        </div>
+        <h3 class="featured_profile-account-name">John Doe</h3>
+        <div class="featured_profile-account-accountInfo">
+          <p>
+            <a href="#" class="featured_profile-account-accountInfo_adds">2 advertenties</a> in <a href="#" class="featured_profile-account-accountInfo_place">plaatsnaam</a>
+          </p>
+        </div>
+        <div class="featured_profile-account-infoText">
+          <?php $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+          ?>
+          <p>
+            <?php if(strlen($text) > 80){
+              $text = substr($text, 0, 80) . '...';
+            }
+            echo $text;
+            ?>
+          </p>
+        </div>
+        <a class="featured_profile-account-url" href="#">Profiel bekijken</a>
+      </div>
+    </div>
+    <div class="columns small-full medium-third featured_profile-account">
+      <div class="featured_profile-account-container">
+        <div class="featured_profile-account-image">
+          <span class="featured_profile-account-image-number">123</span>
+        </div>
+        <h3 class="featured_profile-account-name">John Doe</h3>
+        <div class="featured_profile-account-accountInfo">
+          <p>
+            <a href="#" class="featured_profile-account-accountInfo_adds">2 advertenties</a> in <a href="#" class="featured_profile-account-accountInfo_place">plaatsnaam</a>
+          </p>
+        </div>
+        <div class="featured_profile-account-infoText">
+          <?php $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+          ?>
+          <p>
+            <?php if(strlen($text) > 80){
+              $text = substr($text, 0, 80) . '...';
+            }
+            echo $text;
+            ?>
+          </p>
+        </div>
+        <a class="featured_profile-account-url" href="#">Profiel bekijken</a>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="recentAdds-discover">
+  <div class="row no-margin">
+    <div class="columns small-full featured_profile-header">
+      <div class="featured_profile-header_container">
+        <p>
+          Uitgelichte opdrachten in "<?php echo $title; ?>"
+        </p>
+        <a href="<?php echo $url; ?>" class="featured_profile-header-url">Categorie bekijken</a>
+      </div>
+    </div>
+  </div>
+  <div class="row no-margin">
+    <?php
+    for ($x = 0; $x < 2; $x++) {
+    ?>
+    <div class="columns small-full medium-half recentAdds-container">
+      <div class="recentAdds-element">
+        <a class="full_url" href="#"></a>
+        <div class="recentAdds-element_image">
+          <span class="recentAdds-element_image-number">123</span>
+        </div>
+        <div class="recentAdds-element_info">
+          <div class="recentAdds-element_info-header">
+            <div class="recentAdds-element_info-header-top">
+              <span class="recentAdds-elements_info-header-cost">Gratis</span>
+              <h2 class="recentAdds-elements_info-header-title">FeelGood opdracht gevraagd</h2>
+            </div>
+            <div class="recentAdds-element_info-header-linkInfo">
+              <p class="recentAdds-element_info-header-links">
+                <a href="#" class="recentAdds-element_info-header-url category">Categorie</a> in <a href="#" class="recentAdds-element_info-header-url place">plaatsnaam</a>
+              </p>
+            </div>
+          </div>
+          <div class="recentAdds-element_info-text">
+            <p class="recentAdds-element_info-text_information">
+              <?php $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+              if(strlen($text) > 150){
+                $text = substr($text, 0, 150) . '...';
+              }
+              echo $text;
+              ?>
+            </p>
+            <p class="recentAdds-element_info-text_date">
+              Geplaatst op 1-1-2018
+            </p>
+          </div>
+        </div>
+      </a>
+      </div>
+    </div>
+    <?php } ?>
+  </div>
+</section>
 <?php  $this->inc('elements/footer.php'); ?>
