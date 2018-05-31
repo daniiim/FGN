9073c6ca12a0d19a56e3900f572e475075e78827
Danilo Merea <36174309+danilomerea@users.noreply.github.com>
2018-05-31 18:54:58 +0200

    Footer test with chatbox

--- src/elements/footer.php
+++ src/elements/footer.php
@@ -85,5 +85,8 @@ $path = str_replace("/concrete/concrete", "/concrete", $this->getThemePath());
   </div>
 </footer>
 <script src="<?php echo $path; ?>/js/app.js"></script>
+<!-- Begin of Chaport Live Chat code -->
+<script type="text/javascript"> (function(w,d,v2){ w.chaport = { app_id : '5a5de79ea1e8df39a25639cc' }; v2=w.chaport;v2._q=[];v2._l={};v2.q=function(){v2._q.push(arguments)};v2.on=function(e,fn){if(!v2._l[e])v2._l[e]=[];v2._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document); </script>
+<!-- End of Chaport Live Chat code -->
 <?php Loader::element('footer_required'); ?>
 </body></html>
